78f025b7f7f5de0f149d86dc5c9ce047c381029e
Jeremias <jmb.idev@gmail.com>
2019-12-02 12:54:18 -0300

    [JB][CHANGED][Removed unnecessary try catch block]

--- src/screens/MainApp/Settings/index.js
+++ src/screens/MainApp/Settings/index.js
@@ -8,13 +8,8 @@ import MessagingService from '~/messagingService'
 
 const LogOut = async () => {
   try {
-    await MessagingService.deleteFCMTokenForCurrentUser()
-  } catch (error) {
-    console.log(error.message)
-  }
-
-  try {
     await firebase.auth().signOut()
+    await MessagingService.deleteFCMTokenForCurrentUser()
     NavigationService.navigate('Login')
   } catch (error) {
     console.log(error.message)
