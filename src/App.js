60fe84a1027553944d77e88e17a02a7c5d95eed0
Jeremias <jmb.idev@gmail.com>
2019-12-18 20:45:29 -0300

    [JB][CHANGED] Bottom tab bar modification according to wireframes

--- src/App.js
+++ src/App.js
@@ -1,78 +1,23 @@
 import fixtimerbug from '~/bugFixes/fixtimerbug' // this is just an import to load the fix
 import React from 'react'
 import { createAppContainer, createSwitchNavigator } from 'react-navigation'
-import { createBottomTabNavigator } from 'react-navigation-tabs'
 import * as firebase from 'firebase'
 import 'firebase/firestore'
-import Icon from 'react-native-vector-icons/FontAwesome5'
 import firebaseConfig from './configs/firebase'
 import NavigationService from './navigationService'
 import SignUp from './screens/authentication/SignUp'
 import Login from './screens/authentication/Login'
 import LoginEmail from './screens/authentication/LoginEmail'
 import UsernameRequest from './screens/authentication/UsernameRequest'
-import Home from './screens/MainApp/Home'
 import LoadingScreen from './screens/LoadingScreen'
 import Onboarding from './screens/Onboarding'
-import DiagnoseRequest from './screens/MainApp/DiagnoseRequest'
-import DiagnoseResponse from './screens/MainApp/DiagnoseResponse'
-import Settings from './screens/MainApp/Settings'
-import styles from './styles'
+import MainApp from './screens/MainApp'
 
 // console.disableYellowBox = true // releases only
 
 if (firebase.apps.length === 0) {
   firebase.initializeApp(firebaseConfig)
 }
-
-const MainApp = createBottomTabNavigator({
-  Home: {
-    screen: Home,
-    navigationOptions: {
-      headerTitle: 'Home',
-      tabBarLabel: 'Diagnósticos',
-      tabBarIcon: ({ tintColor }) => (
-        <Icon name='cannabis' size={18} color={tintColor} />
-      )
-    }
-  },
-  DiagnoseRequest: {
-    screen: DiagnoseRequest,
-    navigationOptions: {
-      headerTitle: 'DiagnoseRequest',
-      tabBarLabel: 'Diagnosticar',
-      tabBarIcon: ({ tintColor }) => (
-        <Icon name='comment-medical' size={18} color={tintColor} />
-      )
-    }
-  },
-  DiagnoseResponse: {
-    screen: DiagnoseResponse,
-    navigationOptions: {
-      headerTitle: 'DiagnoseResponse',
-      tabBarLabel: 'Respuestas',
-      tabBarIcon: ({ tintColor }) => (
-        <Icon name='microscope' size={18} color={tintColor} />
-      )
-    }
-  },
-  Settings: {
-    screen: Settings,
-    navigationOptions: {
-      headerTitle: 'Settings',
-      tabBarLabel: 'Opciones',
-      tabBarIcon: ({ tintColor }) => (
-        <Icon name='cog' size={18} color={tintColor} />
-      )
-    }
-  }
-}, {
-  tabBarOptions: {
-    activeTintColor: 'green',
-    inactiveTintColor: 'gray',
-    style: styles.tabBarStyle
-  }
-})
 
 const MainNavigator = createSwitchNavigator({
   LoadingScreen: { screen: LoadingScreen },
