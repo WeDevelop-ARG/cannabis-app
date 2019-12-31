5373edeaa38b1d9b0ff86655dd36393b59d33a40
MPucheta <smpucheta@gmail.com>
2019-12-30 22:15:13 -0300

    Adapt theme to current styleguide (colors, fonts, sizes)

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -2,64 +2,63 @@ import { scale, verticalScale, moderateScale } from 'react-native-size-matters'
 import { widthPercentageToDP } from 'react-native-responsive-screen';
 
 const colors = {
-  primary: 'rgba(92, 254, 78, 0.71)',
-  secondary: 'rgba(254,93,78, 0.8)',
-  black: '#2F2F2F',
+  primary: '#5ECE84',
+  secondary: '#FE9458',
+  black: '#4B5057',
   white: '#FFFFFF',
-  gray: '#BDBFC7',
-  gray2: '#D8D8D8',
-  gray3: '#F0F0F0',
+  gray: '#A6AFBC',
+  gray2: '#D3D5D8',
+  gray3: '#FAFAFA',
   alpha: 'rgba(255, 255, 255, 0.01)'
 }
 
 const sizes = {
   // global sizes
-  base: moderateScale(20),
-  font: moderateScale(14),
-  border: moderateScale(30),
-  padding: moderateScale(10),
-  margin: moderateScale(15),
+  base: moderateScale(40, 0.25),
+  font: moderateScale(14, 0.25),
+  border: moderateScale(7, 0.25),
+  padding: moderateScale(12, 0.25),
+  margin: moderateScale(24, 0.25),
   containerWidth: widthPercentageToDP('88%'),
 
   // font sizes
-  h1: moderateScale(22),
-  h2: moderateScale(18),
-  h3: moderateScale(14),
-  description: moderateScale(16),
-  body: moderateScale(14),
-  date: moderateScale(12)
+  h1: moderateScale(20, 0.25),
+  h2: moderateScale(18, 0.25),
+  h3: moderateScale(16, 0.25),
+  h4: moderateScale(14, 0.25),
+  description: moderateScale(16, 0.25),
+  body: moderateScale(14, 0.25),
+  date: verticalScale(12)
 }
 
 const fonts = {
   h1: {
-    fontFamily: 'Roboto',
-    fontSize: sizes.h1,
-    fontWeight: 'bold'
+    fontFamily: 'raleway-bold',
+    fontSize: sizes.h1
   },
   h2: {
-    fontFamily: 'Roboto',
-    fontSize: sizes.h2,
-    fontWeight: 'bold'
+    fontFamily: 'raleway-bold',
+    fontSize: sizes.h2
   },
   h3: {
-    fontFamily: 'Roboto',
-    fontSize: sizes.h3,
-    fontWeight: 'bold'
+    fontFamily: 'raleway-bold',
+    fontSize: sizes.h3
+  },
+  h4: {
+    fontFamily: 'raleway-bold',
+    fontSize: sizes.h4
   },
   description: {
-    fontFamily: 'Roboto',
-    fontSize: sizes.description,
-    fontWeight: 'normal'
+    fontFamily: 'red-hat-display-regular',
+    fontSize: sizes.description
   },
   body: {
-    fontFamily: 'Roboto',
-    fontSize: sizes.body,
-    fontWeight: 'normal'
+    fontFamily: 'red-hat-display-regular',
+    fontSize: sizes.body
   },
   date: {
-    fontFamily: 'Roboto',
-    fontSize: sizes.date,
-    fontWeight: '500'
+    fontFamily: 'red-hat-display-medium',
+    fontSize: sizes.date
   }
 }
 
