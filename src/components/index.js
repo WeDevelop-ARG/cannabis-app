d47b80c8d6a7719c1f901c70eacbee3c47ae393c
MPucheta <smpucheta@gmail.com>
2020-01-02 12:34:20 -0300

    Compact exports in components

--- src/components/index.js
+++ src/components/index.js
@@ -1,22 +1,4 @@
-export {
-  Text,
-  Title,
-  Subtitle,
-  Header,
-  Subheader,
-  Description,
-  Body,
-  Date
-} from './texts'
-
-export {
-  Button,
-  GrayButton,
-  PrimaryButton,
-  AlphaButton,
-  BlackButton,
-  ImageButton
-} from './buttons'
-
+export * from './texts'
+export * from './buttons'
 export { default as RealThumbnailOrPlaceholder } from './RealThumbnailOrPlaceholder'
 export { default as ImageSelection } from './ImageSelection'
