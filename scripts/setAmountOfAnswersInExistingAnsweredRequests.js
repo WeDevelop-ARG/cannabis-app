6b3db7ed96cb8b0c1bc207babb13b08170203873
MPucheta <smpucheta@gmail.com>
2020-01-24 11:30:13 -0300

    Calculate amountOfAnswers based on existing responses

--- scripts/setAmountOfAnswersInExistingAnsweredRequests.js
+++ scripts/setAmountOfAnswersInExistingAnsweredRequests.js
@@ -15,8 +15,14 @@ const setAmountOfAnswersInExistingAnsweredRequests = async () => {
     await Promise.all(
       snapshots.docs.map(async (doc) => {
         console.log(`Setting amount of answers for ${doc.id}`)
+
+        const responses = await admin
+          .firestore()
+          .collection(`users/${doc.ref.parent.parent.id}/requests/${doc.id}/responses`)
+          .get()
+
         await doc.ref.update({
-          amountOfAnswers: 1
+          amountOfAnswers: responses.docs.length
         })
       })
     )
