a9c62152ee60b68fb893cf3e2bd91ac544a8c4bd
Jeremias <jmb.idev@gmail.com>
2020-01-29 11:59:36 -0300

    Redirect to MyDiagnoses after notification open by user

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import fixtimerbug from '~/bugFixes/fixtimerbug' // this is just an import to load the fix
-import React from 'react'
+import React, { useEffect } from 'react'
 import { createAppContainer, createSwitchNavigator } from 'react-navigation'
 import * as firebase from 'firebase'
 import 'firebase/firestore'
@@ -14,6 +14,8 @@ import Onboarding from './screens/Onboarding'
 import MainApp from './screens/MainApp'
 import PrivacyPolicy from './screens/PrivacyPolicy'
 import { buildStackNavigator } from '~/components/StackNavigator'
+import notificationService from '~/notificationService'
+import navigationService from './navigationService'
 
 if (!__DEV__) {
   console.disableYellowBox = true
@@ -42,6 +44,9 @@ const MainNavigator = createSwitchNavigator(
 )
 
 const AppContainer = createAppContainer(MainNavigator)
+
+notificationService.configure()
+notificationService.setNotificationHandler(() => navigationService.navigate('MyDiagnoses'))
 
 const App = () => (
   <>
---
+++ src/notificationService/index.js
@@ -0,0 +1,67 @@
+import { PushNotificationIOS } from 'react-native'
+import PushNotification from 'react-native-push-notification'
+
+const _notifications = []
+let _handlerEnabled = true
+let _notificationsHandler = (notification) => {}
+
+const _options = {
+  onRegister: (token) => {
+    console.log('TOKEN:', token)
+  },
+  onNotification: (notification) => {
+    if (notification.userInteraction && _handlerEnabled) {
+      _notificationsHandler(notification)
+    }
+    notification.finish(PushNotificationIOS.FetchResult.NoData)
+  },
+  permissions: {
+    alert: true,
+    badge: true,
+    sound: true
+  },
+  // Should the initial notification be popped automatically
+  // default: true
+  popInitialNotification: false,
+  /**
+   * (optional) default: true
+   * - Specified if permissions (ios) and token (android and ios) will requested or not,
+   * - if not, you must call PushNotificationsHandler.requestPermissions() later
+   */
+  requestPermissions: true
+}
+
+export const configure = (options) => {
+  PushNotification.configure({
+    ..._options,
+    ...options
+  })
+
+  PushNotification.popInitialNotification((notification) => {
+    if (notification) { _notifications.push(notification) }
+  })
+}
+
+export const setNotificationHandler = (notificationHandler) => {
+  _notificationsHandler = notificationHandler
+}
+
+export const enableHandler = () => {
+  _handlerEnabled = true
+}
+
+export const disableHandler = () => {
+  _handlerEnabled = false
+}
+
+export const popInitialNotification = () => {
+  return _notifications.shift()
+}
+
+export default {
+  configure,
+  setNotificationHandler,
+  enableHandler,
+  disableHandler,
+  popInitialNotification
+}
